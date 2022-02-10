e5b037a865d9e48d5647f72b3cff08ef13cb0b59
Joe <83708577+Joseph-P-OToole@users.noreply.github.com>
2022-02-10 09:05:10 -0500

    skill images

--- Data/SkillData.cs
+++ Data/SkillData.cs
@@ -1,23 +1,23 @@
 
 public class SkillData {
-    public string[] skillNames = {"python", "js", "c-sharp", ".net core", "blazor", "java", "android", "php", "ios", "adobe illustrator", "adobe photoshop", "react", "node js", "sql server", "sql lite"};
+    public string[] skillNames = {"python", "js", "c-sharp", ".net core", "blazor", "java", "android", "php", "ios", "adobe illustrator", "adobe photoshop", "react", "node js", "SQL server", "SQL lite"};
     public string[] skillDescriptions = {"This is a template generator I built in Python using Tkinter, users can choose between an empty boilerplate, a canvas and animation frame boilerplate, or a full website template with many common features. It then copies the selected template to the specified directory.",
-                                        "This is a game I built in vanilla Js. The object of the game is to collect gas cans while avoiding the asteroids. The rocket is being pulled toward a black hole and the user must use fuel in order to collect more.",
+                                        "This is a game I built in vanilla Javascript. The object of the game is to collect gas cans while avoiding the asteroids. The rocket is being pulled toward a black hole and the user must use fuel in order to collect more.",
                                         "This is a movie database website I built in .NET Core. It is retrieving data from The Movie Database via an API. Users can search a movie title or actor name to view trailers, movie info, actor biography and social media links.",
                                         "This is an automotive training tool I built using .NET core and adobe illustrator. It uses a SQL server database and custom SVG graphics to aid in learning about automotive systems.",
-                                        "This website was created using Blazor web assembly. It is styled with custom css on-top of the bootstrap boilerplate and includes custom css animations.",
+                                        "This website was created using Blazor web assembly. It is styled with custom CSS on-top of the bootstrap boilerplate and includes custom css animations.",
                                         "This is an automotive troubleshooting tool I built in Java. It has a few diagnostic paths with basic troubleshooting questions that will lead the user to primary checks and a safety advisory based on the conditions.",
                                         "This is a simple metronome app I built for Android. Users can adjust for tempo as well as for time signature. There is built in tone used for tuning and italian tempo markings. It was written using Android Studio in Java.",
-                                        "This is a raffle website I built in Php using a Sql database. Users can post items they would like to raffle providing the item value and amount of tickets to be sold. All of the data is being stored and retrieved via Sql queries.",
-                                        "This is a coffee order app I built in IOS using Swift and Sql Lite. It allows users to create coffee orders with common drinks and custom ingredients. Users can also nickname and save the orders.",
-                                        "This is a logo I made using Adobe Illustrator.",
+                                        "This is a raffle website I built in Php using a SQL database. Users can post items they would like to raffle providing the item value and amount of tickets to be sold. All of the data is being stored and retrieved via SQL queries.",
+                                        "This is a coffee order app I built in IOS using Swift and SQL Lite. It allows users to create coffee orders with common drinks and custom ingredients. Users can also nickname and save the orders.",
+                                        "This is a diagram of a car I created with Adobe Illustrator. It is being used as a training tool for identifying and learning about automotive systems.",
                                         "This is a website mockup I made using Adobe Photoshop. This mockup was based on a wire frame sketch provided as a class assignment.",
-                                        "This is an older version of my portfolio created with React. It is a single page application that displays my skills and projects.",
+                                        "This is an older version of my portfolio created using React. It is a single page application that displays my skills and projects.",
                                         "This is a joke website I built using NodeJs and Express. It is retrieving random jokes via the Chuck Norris API and randomly selecting a background image of the legend himself.",
-                                        "This is a raffle website I built in Php using a Sql database. Users can post items they would like to raffle providing the item value and amount of tickets to be sold. All of the data is being stored and retrieved via Sql queries.",
-                                        "sql lite"};
-    public string[] skillIcons = {"python-icon", "js-icon", "cs-icon", "dot-net-core-icon", "blazor-icon", "java-icon", "android-icon", "php-icon", "ios-icon", "adobe-illustrator-icon", "adobe-photoshop-icon", "react-icon", "node-js-icon", "sql-icon", "sql-lite-icon"};
-    public string[] skillImages = {"python-image", "js-image", "cs-image", "dot-net-core-image", "blazor-image", "java-image", "android-image", "php-image", "ios-image", "adobe-illustrator-image", "adobe-photoshop-image", "react-image", "node-js-image", "sql-image", "sql-lite-image"};
+                                        "This is a raffle website I built in Php using a SQL database. Users can post items they would like to raffle providing the item value and amount of tickets to be sold. All of the data is being stored and retrieved via SQL queries.",
+                                        "This is a coffee order app I built in IOS using Swift and SQL Lite. It allows users to create coffee orders with common drinks and custom ingredients. Users can also nickname and save the orders."};
+    public string[] skillIcons = {"python-icon", "js-icon", "cs-icon", "dot-net-core-icon", "blazor-icon", "java-icon", "android-icon", "php-icon", "ios-icon", "adobe-illustrator-icon", "adobe-photoshop-icon", "react-icon", "node-js-icon", "SQL-icon", "SQL-lite-icon"};
+    public string[] skillImages = {"python-image", "js-image", "cs-image", "dot-net-core-image", "blazor-image", "java-image", "android-image", "php-image", "ios-image", "adobe-illustrator-image", "adobe-photoshop-image", "react-image", "node-js-image", "SQL-image", "SQL-lite-image"};
     
     public string[] skillLinks = {"", "https://joseph-p-otoole.github.io/blackholeGame/", "", "https://automotiveta.com/", "" , "", "", "", "", "", "", "https://joseph-p-otoole.github.io/portfolio/", "", "", ""};
 
